889075ceac555eb8fbe5350e1519f0f36f43ab32
codeNut247 <codeNut247@gmail.com>
2017-04-04 14:19:08 +0200

    SignIn-Experimentation

--- main.go
+++ main.go
@@ -9,7 +9,18 @@ import (
 type pageData struct {
 	Title     string
 	FirstName string
+	User      user
 }
+
+type user struct {
+	UserName  string
+	Email     string
+	Password  string
+	Remember  bool
+	IsLogedIn bool
+}
+
+var pd pageData
 
 var tpl *template.Template
 
@@ -22,44 +33,61 @@ func main() {
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/contact", contact)
 	http.HandleFunc("/apply", apply)
+	http.HandleFunc("/signin", signin)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
 	http.ListenAndServe(":8080", nil)
 }
 
-func sendView(w http.ResponseWriter, pd pageData) {
+func sendView(w http.ResponseWriter, tempName string, pd pageData) {
 	w.Header().Set("Content-Type", "text/html")
-	err := tpl.ExecuteTemplate(w, "templist.gohtml", pd)
+	err := tpl.ExecuteTemplate(w, tempName, pd)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
+}
+
+func signin(w http.ResponseWriter, req *http.Request) {
+	//pd = pageData{Title: "Sign Up"}
+	pd.Title = "Sign In"
+	if req.Method == http.MethodPost {
+		pd.User.UserName = req.FormValue("username")
+		pd.User.Email = req.FormValue("email")
+		pd.User.Password = req.FormValue("password")
+		if req.FormValue("remember") == "on" {
+			pd.User.Remember = true
+		}
+		pd.User.IsLogedIn = true
+		log.Println(pd.User)
+		log.Println(req.FormValue("remember"))
+	}
+	sendView(w, "signinCreate.gohtml", pd)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	pd := pageData{Title: "Index"}
-	sendView(w, pd)
+	//pd = pageData{Title: "Index"}
+	pd.Title = "Index"
+	sendView(w, "templist.gohtml", pd)
 }
 
 func about(w http.ResponseWriter, req *http.Request) {
-	pd := pageData{Title: "About"}
-	sendView(w, pd)
+	//pd = pageData{Title: "About"}
+	pd.Title = "About"
+	sendView(w, "templist.gohtml", pd)
 }
 
 func contact(w http.ResponseWriter, req *http.Request) {
-	pd := pageData{Title: "Contact"}
-	sendView(w, pd)
+	//pd = pageData{Title: "Contact"}
+	pd.Title = "Contact"
+	sendView(w, "templist.gohtml", pd)
 }
 
 func apply(w http.ResponseWriter, req *http.Request) {
-	pd := pageData{Title: "Apply"}
-	w.Header().Set("Content-Type", "text/html")
+	//pd = pageData{Title: "Apply"}
+	pd.Title = "Apply"
 	if req.Method == http.MethodPost {
 		pd.FirstName = req.FormValue("fname")
 	}
-	err := tpl.ExecuteTemplate(w, "createview.gohtml", pd)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
+	sendView(w, "createview.gohtml", pd)
 }
